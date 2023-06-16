db86838d5eb494af8e7412a1797d5b5a64a2097d
Anton_Fursov <fursila02@mail.ru>
2023-06-16 14:12:34 +0300

    Homework

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2){ 
+    Console.WriteLine($"Max:{num1}");
+    Console.WriteLine($"Min:{num2}");
+}
+else if (num1 < num2){ 
+    Console.WriteLine($"Max: {num2}");
+    Console.WriteLine($"Min: {num1}");
+}
+else { 
+    Console.WriteLine("числа равны");
+}
+
